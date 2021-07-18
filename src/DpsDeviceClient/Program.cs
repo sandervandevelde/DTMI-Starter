56073495eb8ce4b2f350860b88e390e77a91498a
Sander van de Velde <svelde@gmail.com>
2021-07-18 14:28:11 +0200

    Added clients

---
+++ src/DpsDeviceClient/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using System.Text;
+
+namespace DpsDeviceClient
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Sample taken from https://github.com/Azure-Samples/azure-iot-samples-csharp/blob/master/provisioning/Samples/device/SymmetricKeySample/Program.cs
+        /// </summary>
+        /// <param name="args"></param>
+        public static async Task<int> Main(string[] args)
+        {
+            var parameters = new Parameters
+            {
+                GlobalDeviceEndpoint = "global.azure-devices-provisioning.net",
+                IdScope = "0ne0003BB8B",
+                Id = "PnPDeviceViaDps",
+                PrimaryKey = "SECRET",
+            };
+
+            await RunSampleAsync(parameters);
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+
+            return 0;
+        }
+
+        private static async Task RunSampleAsync(Parameters parameters)
+        {
+            Console.WriteLine($"Initializing the device provisioning client...");
+
+            // For individual enrollments, the first parameter must be the registration Id, where in the enrollment
+            // the device Id is already chosen. However, for group enrollments the device Id can be requested by
+            // the device, as long as the key has been computed using that value.
+            // Also, the secondary could be included, but was left out for the simplicity of this sample.
+            using var security = new SecurityProviderSymmetricKey(
+                parameters.Id,
+                parameters.PrimaryKey,
+                null);
+
+            using var transportHandler = new ProvisioningTransportHandlerMqtt(TransportFallbackType.TcpOnly);
+
+            ProvisioningDeviceClient provClient = ProvisioningDeviceClient.Create(
+                parameters.GlobalDeviceEndpoint,
+                parameters.IdScope,
+                security,
+                transportHandler);
+
+            Console.WriteLine($"Initialized for registration Id {security.GetRegistrationID()}.");
+
+            Console.WriteLine("Registering with the device provisioning service...");
+            DeviceRegistrationResult result = await provClient.RegisterAsync();
+
+            Console.WriteLine($"Registration status: {result.Status}.");
+
+            if (result.Status != ProvisioningRegistrationStatusType.Assigned)
+            {
+                Console.WriteLine($"Registration status did not assign a hub, so exiting this sample.");
+                return;
+            }
+
+            Console.WriteLine($"Device {result.DeviceId} registered to {result.AssignedHub}.");
+
+            Console.WriteLine("Creating symmetric key authentication for IoT Hub...");
+
+            IAuthenticationMethod auth = new DeviceAuthenticationWithRegistrySymmetricKey(
+                result.DeviceId,
+                security.GetPrimaryKey());
+
+            Console.WriteLine($"Testing the provisioned device with IoT Hub...");
+
+            using DeviceClient iotClient = DeviceClient.Create(
+                result.AssignedHub,
+                auth,
+                new ClientOptions { ModelId = "dtmi:com:example:NotExisting;2" });
+
+            Console.WriteLine("Sending a telemetry message...");
+
+            string jsonData = "{ \"meaning\":43}";
+
+            using var message = new Message(Encoding.UTF8.GetBytes(jsonData));
+
+            await iotClient.SendEventAsync(message);
+
+            Console.WriteLine("Finished.");
+        }
+    }
+   
+    internal class Parameters
+    {
+        public string IdScope { get; set; }
+
+        public string Id { get; set; }
+
+        public string PrimaryKey { get; set; }
+
+        public string GlobalDeviceEndpoint { get; set; }
+    }
+}
--- src/dtmitester/Program.cs
+++ src/dtmitester/Program.cs
@@ -8,99 +8,95 @@ using Azure.Core.Diagnostics;
 
 namespace dtmitest
 {
+    /// <summary>
+    /// Nuget: https://www.nuget.org/packages/Azure.IoT.ModelsRepository
+    /// Nuget code samples: https://github.com/Azure/azure-sdk-for-net/tree/main/sdk/modelsrepository/Azure.IoT.ModelsRepository/samples
+    /// </summary>
     internal class Program
     {
         private static async Task Main(string[] args)
         {
             Console.WriteLine("Hello DTMI World!");
 
-            //using AzureEventSourceListener listener = AzureEventSourceListener.CreateConsoleLogger();
-
-            var clientRaw = new ModelsRepositoryClient(
-                new Uri("https://raw.githubusercontent.com/sandervandevelde/DTMI-Starter/master"),
-                //new Uri("https://raw.githubusercontent.com/Azure/iot-plugandplay-models/main"),
-                new ModelsRepositoryClientOptions(dependencyResolution: ModelDependencyResolution.Enabled));
-
-            IDictionary<string, string> models = clientRaw.GetModels("dtmi:com:example:TemperatureController;1");
-
-            models.Keys.ToList().ForEach(k => Console.WriteLine(k));
-
-            //var client = new ModelsRepositoryClient();
-            //Console.WriteLine($"Initialized client pointing to global endpoint: {client.RepositoryUri}");
+            // note: 'master' is the publicly available branch
+            var uri = new Uri("https://raw.githubusercontent.com/sandervandevelde/DTMI-Starter/master");
 
             var dtmi = "dtmi:com:example:TemperatureController;1";
-            //IDictionary<string, string> models = await client.GetModelsAsync(dtmi).ConfigureAwait(false);
 
-            //// In this case the above dtmi has 2 model dependencies.
-            //// dtmi:com:example:Thermostat;1 and dtmi:azure:DeviceManagement:DeviceInformation;1
-            //Console.WriteLine($"{dtmi} resolved in {models.Count} interfaces.");
+            var isValidDtmi = DtmiConventions.IsValidDtmi(dtmi);
 
-            //foreach (var m in models)
-            //{
-            //    Console.WriteLine($"Model {m.Key}");
-            //}
+            Console.WriteLine($"isValidDtmi: {isValidDtmi}");
+
+            var modelUri = DtmiConventions.GetModelUri(dtmi, uri, false);
+
+            var clientRaw = new ModelsRepositoryClient(
+                    uri,
+                    new ModelsRepositoryClientOptions(dependencyResolution: ModelDependencyResolution.Enabled));
+
+            IDictionary<string, string> models = clientRaw.GetModels(dtmi);
 
             var parser = new ModelParser();
             IReadOnlyDictionary<Dtmi, DTEntityInfo> parseResult = await parser.ParseAsync(models.Values.ToArray());
-            Console.WriteLine($"{dtmi} resolved in {models.Count} interfaces with {parseResult.Count} entities.");
 
-            foreach (var p in parseResult)
-            {
-                Console.WriteLine($"Model {p.Value.GetType()}");
-            }
+            Console.WriteLine($"The following {models.Count} interfaces are resolved with {parseResult.Count} entities.");
 
+            models.Keys.ToList().ForEach(k => Console.WriteLine($"Model: {k}"));
+
+            parseResult.Values.ToList().ForEach(v => Console.WriteLine($"element: {v.GetType()}"));
+
+            //// *** Alternative way to parse. Notice the additional extension method in this project ***
             //var parser = new ModelParser
             //{
             //    // Usage of the ModelsRepositoryClientExtensions.ParserDtmiResolver extension.
             //    DtmiResolver = client.ParserDtmiResolver
             //};
-
             //IReadOnlyDictionary<Dtmi, DTEntityInfo> parseResult = await parser.ParseAsync(models.Values.Take(1).ToArray());
             //Console.WriteLine($"{dtmi} resolved in {models.Count} interfaces with {parseResult.Count} entities."); // 38 entities
 
             foreach (var p in parseResult)
             {
-                if (p.Value is Microsoft.Azure.DigitalTwins.Parser.DTTelemetryInfo)
+                if (p.Value is DTTelemetryInfo)
                 {
-                    System.Console.WriteLine($"parse key '{p.Key}' ");// : '{p.Value}' Id {p.Value.Id.AbsolutePath} DisplayName {p.Value.DisplayName.First().Key} {p.Value.DisplayName.First().Value}");
-                    // System.Console.WriteLine($"  EntityKind {p.Value.EntityKind.ToString()}");
-                    // //System.Console.WriteLine($"  Description {p.Value.Description.First()}");
-                    System.Console.WriteLine($"  DefinedIn {p.Value.DefinedIn.AbsolutePath}");
-                    // ////System.Console.WriteLine($"Comment {p.Value.Comment.First()}");
+                    Console.WriteLine($"parse '{p.Value}': '{p.Key}' ");
+                    Console.WriteLine($"  Id '{p.Value.Id.AbsolutePath}' ");
+                    Console.WriteLine($"  DisplayName '{p.Value.DisplayName.First().Key}' - '{p.Value.DisplayName.First().Value}'");
+                    Console.WriteLine($"  EntityKind {p.Value.EntityKind}");
+                    Console.WriteLine($"  Description {p.Value.Description.First()}");
+                    Console.WriteLine($"  DefinedIn {p.Value.DefinedIn.AbsolutePath}");
+                    Console.WriteLine($"  Comment {p.Value.Comment?.First()}");
 
-                    var pt = p.Value as Microsoft.Azure.DigitalTwins.Parser.DTTelemetryInfo;
+                    var pt = p.Value as DTTelemetryInfo;
+                    Console.WriteLine($"  pt.EntityKind = {pt.EntityKind}");
 
-                    System.Console.WriteLine($"pt.EntityKind = {pt.EntityKind}");
-
-                    if (pt.Schema is Microsoft.Azure.DigitalTwins.Parser.DTDoubleInfo)
+                    if (pt.Schema is DTDoubleInfo)
                     {
-                        var ptdouble = pt.Schema as Microsoft.Azure.DigitalTwins.Parser.DTDoubleInfo;
+                        var ptdouble = pt.Schema as DTDoubleInfo;
 
-                        System.Console.WriteLine($"ptdouble {ptdouble.EntityKind} - {ptdouble.Description.First().Value} ");
+                        Console.WriteLine($"  ptdouble {ptdouble.EntityKind} - {ptdouble.Description.First().Value} ");
                     }
                 }
 
-                // if (p.Value is Microsoft.Azure.DigitalTwins.Parser.DTCommandInfo)
+                // if (p.Value is DTCommandInfo)
                 // {
-                //     System.Console.WriteLine($"parse key {p.Key} {p.Value} Id {p.Value.Id.AbsolutePath} DisplayName {p.Value.DisplayName.First().Key} {p.Value.DisplayName.First().Value}");
-                //     System.Console.WriteLine($"  EntityKind {p.Value.EntityKind.ToString()}");
-                //     //System.Console.WriteLine($"  Description {p.Value.Description.First()}");
-                //     System.Console.WriteLine($"  DefinedIn {p.Value.DefinedIn.AbsolutePath}");
-                //     ////System.Console.WriteLine($"Comment {p.Value.Comment.First()}");
+                //     Console.WriteLine($"parse key {p.Key} {p.Value} Id {p.Value.Id.AbsolutePath} DisplayName {p.Value.DisplayName.First().Key} {p.Value.DisplayName.First().Value}");
+                //     Console.WriteLine($"  EntityKind {p.Value.EntityKind.ToString()}");
+                //     Console.WriteLine($"  Description {p.Value.Description.First()}");
+                //     Console.WriteLine($"  DefinedIn {p.Value.DefinedIn.AbsolutePath}");
+                //     Console.WriteLine($"  Comment {p.Value.Comment?.First()}");
                 // }
 
-                // if (p.Value is Microsoft.Azure.DigitalTwins.Parser.DTPropertyInfo)
+                // if (p.Value is DTPropertyInfo)
                 // {
-                //     System.Console.WriteLine($"parse key {p.Key} {p.Value} Id {p.Value.Id.AbsolutePath} DisplayName {p.Value.DisplayName.First().Key} {p.Value.DisplayName.First().Value}");
-                //     System.Console.WriteLine($"  EntityKind {p.Value.EntityKind.ToString()}");
-                //     //System.Console.WriteLine($"  Description {p.Value.Description.First()}");
-                //     System.Console.WriteLine($"  DefinedIn {p.Value.DefinedIn.AbsolutePath}");
-                //     ////System.Console.WriteLine($"Comment {p.Value.Comment.First()}");
+                //     Console.WriteLine($"parse key {p.Key} {p.Value} Id {p.Value.Id.AbsolutePath} DisplayName {p.Value.DisplayName.First().Key} {p.Value.DisplayName.First().Value}");
+                //     Console.WriteLine($"  EntityKind {p.Value.EntityKind.ToString()}");
+                //     Console.WriteLine($"  Description {p.Value.Description.First()}");
+                //     Console.WriteLine($"  DefinedIn {p.Value.DefinedIn.AbsolutePath}");
+                //     Console.WriteLine($"  Comment {p.Value.Comment?.First()}");
                 // }
             }
 
-            //System.Console.WriteLine("Press a key to exit");
-            //System.Console.ReadKey();
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
         }
     }
 }
